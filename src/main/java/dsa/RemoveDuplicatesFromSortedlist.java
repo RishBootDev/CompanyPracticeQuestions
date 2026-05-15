38fb4fb9a8890876e67b211187c71b9194422529
Rishabh <rishabhdubey1472004@gmail.com>
2026-05-15 07:32:57 +0530

    solved some questions

--- src/main/java/dsa/RemoveDuplicatesFromSortedlist.java
+++ src/main/java/dsa/RemoveDuplicatesFromSortedlist.java
@@ -1,4 +1,23 @@
 package dsa;
 
 public class RemoveDuplicatesFromSortedlist {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+    }
+
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode current = head;
+            while (current != null && current.next != null) {
+                if (current.val == current.next.val) {
+                    current.next = current.next.next;
+                } else {
+                    current = current.next;
+                }
+            }
+            return head;
+        }
+
 }
